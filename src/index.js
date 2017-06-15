17313ad91701c1bc6d22f58f4e43e5788f0ce525
Paulo Ragonha <paulo@ragonha.me>
2017-06-15 13:37:38 +0200

    The simplest Redux app 🌈

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import style from './index.css'
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { connect, Provider } from 'react-redux'
 
-const div = document.createElement('div')
-div.innerHTML = '<h1>Hello World</h1>'
-div.className = style.component
+const initialState = { yay: 1 }
 
-document.getElementById('root').appendChild(div)
+const rootReducer = (state = initialState, action) => state
+
+const store = createStore(rootReducer)
+
+const mapStateToProps = state => state
+
+const App = connect(mapStateToProps)(props => <h1>Hello {props.yay}</h1>)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
